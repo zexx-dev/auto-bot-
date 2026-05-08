60b0366e7d96cd37a5d8dde88651a75765d13bb6
zexx-dev <padmabatibhol8@gmail.com>
2026-05-08 15:00:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config:
     # --- REQUIRED ---
-    API_ID = int(os.environ.get("API_ID", "1234567")) # Apna API_ID dalo
-    API_HASH = os.environ.get("API_HASH", "your_hash") # Apna Hash dalo
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_token") # Apna Token dalo
-    ADMIN_ID = int(os.environ.get("ADMIN_ID", "00000000")) # Apni User ID dalo
+    API_ID = int(os.environ.get("API_ID", "22346961")) # Apna API_ID dalo
+    API_HASH = os.environ.get("API_HASH", "d68fc6ff87a69a39efdc1656acd001d4") # Apna Hash dalo
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7848192544:AAEPLywZ09vi4lis2yiYdGvpwotZZpGyxeo") # Apna Token dalo
+    ADMIN_ID = int(os.environ.get("ADMIN_ID", "6715373877")) # Apni User ID dalo
     
     # --- OPTIONAL ---
     DB_NAME = "bot_data.db"
